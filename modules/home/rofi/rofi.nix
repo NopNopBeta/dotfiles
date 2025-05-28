48ea1c806bebf21fd0217361d2540de20ff0c6a1
NopNopBeta <63950989+NopNopBeta@users.noreply.github.com>
2025-05-28 15:56:12 +0700

    change rofi theme

--- modules/home/rofi/rofi.nix
+++ modules/home/rofi/rofi.nix
@@ -3,21 +3,31 @@
 {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland; # atau pkgs.rofi untuk X11
+    package = pkgs.rofi-wayland;
     
     # Gunakan config.rasi yang sudah ada
-    theme = "~/.config/rofi/config.rasi";
+    theme = "~/.config/rofi/launchers/type-7/launcher.sh";
+    
+    # Config tambahan jika diperlukan
+    extraConfig = {
+      # Bisa tambah config khusus di sini jika perlu
+    };
   };
   
-  # Copy semua themes ke ~/.config/rofi
+  # Copy semua themes termasuk config.rasi
   home.file.".config/rofi" = {
     source = ./themes;
     recursive = true;
   };
   
-  # Scripts untuk launcher dan powermenu
-  home.file.".local/bin/rofi-launcher" = {
+  # Scripts untuk berbagai launcher
+  home.file.".local/bin/rofi-launcher-t1" = {
     source = ./themes/scripts/launcher_t1;
+    executable = true;
+  };
+  
+  home.file.".local/bin/rofi-launcher-t2" = {
+    source = ./themes/scripts/launcher_t2;
     executable = true;
   };
   
