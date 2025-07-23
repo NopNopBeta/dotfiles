69a888ee72f338b25090796361b44932b17999d7
NopNopBeta <nopnopbeta@duck.com>
2025-07-23 19:29:55 +0700

    Make system more modular (zsh)

--- modules/system/zsh/ohmyzsh.nix
+++ modules/system/zsh/ohmyzsh.nix
@@ -13,31 +13,31 @@
         "python"
         "history"
       ];
-      theme = "robbyrussell";
+      theme = "powerlevel10k/powerlevel10";
       custom = "$ZSH_CUSTOM"; # atau `null` jika tidak custom
     };
     
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.zsh-autosuggestions;
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.zsh-syntax-highlighting;
-      }
-      {
-        name = "zsh-completions";
-        src = pkgs.zsh-completions;
-      }
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
+    # plugins = [
+    #   {
+    #     name = "zsh-autosuggestions";
+    #     src = pkgs.zsh-autosuggestions;
+    #   }
+    #   {
+    #     name = "zsh-syntax-highlighting";
+    #     src = pkgs.zsh-syntax-highlighting;
+    #   }
+    #   {
+    #     name = "zsh-completions";
+    #     src = pkgs.zsh-completions;
+    #   }
+    #   {
+    #     name = "powerlevel10k";
+    #     src = pkgs.zsh-powerlevel10k;
+    #     file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    #   }
+    # ];
     
-    initExtra = ''
+    promptInit = ''
       # Add powerlevel10k instant prompt
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
--- modules/system/zsh/zsh.nix
+++ modules/system/zsh/zsh.nix
@@ -1,25 +1,27 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    zsh-completions    zsh-powerlevel10k
-  ];
-
+    environment.systemPackages = with pkgs; [
+      zsh
+      zsh-autosuggestions
+      zsh-syntax-highlighting
+      zsh-completions
+      zsh-powerlevel10k
+    ];
+    
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    # autosuggestion.enable = true;
+    # syntaxHighlighting.enable = true;
     
-    initExtra = ''
+    promptInit = ''
       # this acts as your ~/.zshrc
       # uncomment if you want to customize your LS_COLORS
       # https://manpages.ubuntu.com/manpages/plucky/en/man5/dir_colors.5.html
       #LS_COLORS='...'
       #export LS_COLORS
+    
       fastfetch
-
       export TERM=xterm-256color
     '';
   };
