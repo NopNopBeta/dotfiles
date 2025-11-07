3ff94769b6cdae315e0fe1ff66e8777e46ab8579
NopNopBeta <NopNopBeta@duck.com>
2025-11-07 14:31:37 +0700

    Pre-fix currentSystem bug

--- modules/home/zoom/default.nix
+++ modules/home/zoom/default.nix
@@ -13,7 +13,7 @@ let
         sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
       })
       {
-        system = "x86_64-linux";
+        stdenv.hostPlatform.system = "x86_64-linux";
         config.allowUnfree = true;
       };
   pinnedZoom = pinnedZoomPkgs.zoom-us;
